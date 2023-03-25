0c8b09496c8f9619ff896bdae404217eaf349ab3
uoch <chaouch.thameurtc61@gmail.com>
2023-03-25 19:52:49 +0100

    db

--- models/place.py
+++ models/place.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, INTEGER, Float, Table
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy.orm import relationship
+from os import getenv
+metadata = Base.metadata
+place_amenity_table = Table('place_amenity',
+                            metadata, Column('place_id', String(60),
+                                             ForeignKey(
+                                'places.id'),
+                                primary_key=True, nullable=False),
+                            Column('amenity_id', String(60),
+                                   ForeignKey('amenities.id'),
+                                   primary_key=True, nullable=False))
 
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+	__tablename__ = 'places'
+    city_id = Column(String(60),  ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60),  ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(INTEGER, nullable=False, default=0)
+    number_bathrooms = Column(INTEGER, nullable=False, default=0)
+    max_guest = Column(INTEGER, nullable=False, default=0)
+    price_by_night = Column(INTEGER, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
+    
--- models/review.py
+++ models/review.py
@@ -5,6 +5,9 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    __tablename__ = "reviews"
+    place_id = Column(String(1024),  nullable=False)
+    user_id = Column(String(60), ForeignKey("places.id"),  nullable=False)
+    text = Column(String(60), ForeignKey("users.id"), nullable=False)
