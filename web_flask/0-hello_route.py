e5d634850940715fc493766f14bc6fa47205fa71
uoch <chaouch.thameurtc61@gmail.com>
2023-04-10 21:30:58 +0100

    2nd_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+@app.route('/hbnb')
+def hello():
+    return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
