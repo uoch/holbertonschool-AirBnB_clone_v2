a9b78a1280db40c4b24378613bfbf076f3933bdb
uoch <chaouch.thameurtc61@gmail.com>
2023-07-07 18:13:50 +0100

    test

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,11 +2,12 @@
 """
 Write a script that starts a Flask web application
 """
-from flask import Flask ,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-"""The strict_slashes=False argument tells Flask to treat URLs with or without a trailing slash as equivalent."""
+"""The strict_slashes=False argument tells Flask to treat URLs
+with or without a trailing slash as equivalent."""
 
 
 @app.route('/', strict_slashes=False)
@@ -35,9 +36,11 @@ def python_is_cool(text='is cool'):
 def is_number(n):
     return "{} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>')
 def number_temp(n):
     return render_template('5-number.html', number=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
